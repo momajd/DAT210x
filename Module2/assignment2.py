ec71234d0f87552aeb52fe2b3b21a043396153b8
momajd <mo.a.majd@gmail.com>
2016-11-20 12:41:33 -0500

    module2-assignment1

--- Module2/assignment2.py
+++ Module2/assignment2.py
@@ -1,20 +1,13 @@
 import pandas as pd
 
-# TODO: Load up the 'tutorial.csv' dataset
-#
-# .. your code here ..
+# Load up the 'tutorial.csv' dataset
+data = pd.read_csv('Datasets/tutorial.csv')
+
+# Print the results of the .describe() method
+print(data.describe())
 
 
-
-# TODO: Print the results of the .describe() method
-#
-# .. your code here ..
-
-
-
-# TODO: Figure out which indexing method you need to
+# Figure out which indexing method you need to
 # use in order to index your dataframe with: [2:4,'col3']
 # And print the results
-#
-# .. your code here ..
-
+print(data.loc[ 2:4, ['col3']])
